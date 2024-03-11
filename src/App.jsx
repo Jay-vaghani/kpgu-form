edfcd047614bd43679197d15f0148a0241db0a9d
Jay-vaghani <jayvaghani10@gmail.com>
2024-03-11 10:57:39 +0530

    error resolve

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import "./App.css";
-import ContactNavbar from "./components/BootstrapNavbar/ContactNavbar";
 import AdmissionInquiry from "./pages/AdmissionInquiry/AdmissionInquiry";
 
 function App() {
   return (
     <>
-      {/* <ContactNavbar /> */}
       <AdmissionInquiry />
     </>
   );
